3b7317036767b97911804f6f15c13eb5d9d773db
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-05-14 17:22:33 -0300

    feat(starting): create "05-input.py".

---
+++ starting/06-building_a_calculator.py
@@ -0,0 +1,10 @@
+print("==== Calculator ====\n\nWelcome to sum calculator in Python!\nNOTE: Addition calculations only!\n\n")
+
+fist_number = input("Enter the first number: ")
+second_number = input("Enter the second number: ")
+
+result = float(fist_number) + float(second_number)
+
+print("The sum of " + str(fist_number) + " and " + str(second_number) + " is " + str(result) + ".")
+
+# RUN -> python3 starting/06-building_a_calculator.py
