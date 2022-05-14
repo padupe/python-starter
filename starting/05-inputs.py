c652c868fc74a5fdaa0230c73729f9b68eeccb07
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-05-14 17:05:17 -0300

    feat(starting): create "05-inputs.py".

---
+++ starting/05-inputs.py
@@ -0,0 +1,8 @@
+print("==== Inputs ====")
+
+name = input("Digite o seu nome: ")
+age = input("Digite a sua idade: ")
+
+print("Olá " + name + ". Hoje você tem " + age + " anos de idade.\nSeja bem-vindo(a)!")
+
+# Run -> python3 startig/05-inputs.py
