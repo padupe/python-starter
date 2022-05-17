42a008bc4a0f5a706185eedc2f2dc2bec692891b
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-05-17 07:37:08 -0300

    feat(cod3r/aula-01): create directory.

---
+++ cod3r/aula-01/pacote/sub/arquivo.py
@@ -0,0 +1,17 @@
+#!python3
+
+print("Funcionou!")
+
+"""
+Com a extensão CodeRunner instalada, podemos rodar este arquivo:
+
+Ctrl + Alt + n
+"""
+
+# printando uma variável específica no Python
+
+# name -> módulo (nome do arquivo)
+print(__name__)
+
+# package -> pacote
+print(__package__)
